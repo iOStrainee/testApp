ea1bbaedf44df9977b6c57663bffb9baa692b028
Admin <admin@Mac-Admin.local>
2020-01-18 21:07:34 +0600

    create at the two files with names CartModelView and CartTableViewController

---
+++ cart/CartModelView/CartModel.swift
@@ -0,0 +1,13 @@
+//
+//  CartModel.swift
+//  bambook
+//
+//  Created by Admin on 18/01/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class CartModel: NSObject {
+
+}
